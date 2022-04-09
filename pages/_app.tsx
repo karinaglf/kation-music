f5be48a10daa36fea8112965fc43f6405c0c745b
Karina Freitas <karinaglf@gmail.com>
2022-04-09 13:16:05 +0100

    add player container setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,41 +1,44 @@
-import '../styles/globals.css';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-import 'reset-css'; //resets css in all different browsers
+import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "reset-css"; // resets css in all different browsers
+import PlayerLayout from "../components/playerLayout";
 
 const theme = extendTheme({
-	colors: {
-		gray: {
-			100: '#F5F5F5',
-			200: '#EEEEEE',
-			300: '#E0E0E0',
-			400: '#BDBDBD',
-			500: '#9E9E9E',
-			600: '#757575',
-			700: '#616161',
-			800: '#424242',
-			900: '#212121',
-		},
-	},
-	components: {
-		Button: {
-			variants: {
-				link: {
-					':focus': {
-						outline: 'none',
-						boxShadow: 'none',
-					},
-				},
-			},
-		},
-	},
+  colors: {
+    gray: {
+      100: "#F5F5F5",
+      200: "#EEEEEE",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ":focus": {
+            outline: "none",
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  },
 });
 
-function MyApp({ Component, pageProps }) {
-	return (
+const MyApp = ({ Component, pageProps }) => {
+  return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />;
+      <PlayerLayout>
+        <Component {...pageProps} />;
+      </PlayerLayout>
     </ChakraProvider>
-  )
-}
+  );
+};
 
 export default MyApp;
