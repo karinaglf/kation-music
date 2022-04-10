c6d6ade22b7464d116840463c5e48d2f6b988e2e
Karina Freitas <karinaglf@gmail.com>
2022-04-10 02:27:31 +0100

    add getServerSideProps for artists

--- components/gradientLayout.tsx
+++ components/gradientLayout.tsx
@@ -32,6 +32,7 @@ const GradientLayout = ({
           <Text fontSize="x-small">{description}</Text>
         </Box>
       </Flex>
+      <Box paddingY="50px">{children}</Box>
     </Box>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,27 @@
 import GradientLayout from "../components/gradientLayout";
+import prisma from "../lib/prisma";
 
-const Home = () => {
+const Home = ({ artists }) => {
   return (
     <GradientLayout
       color="blue"
       title="Karina Freitas"
       subtitle="profile"
       description="15 public playlists"
-      image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
+      image="https://i.pinimg.com/originals/ba/d4/5a/bad45a40fa6e153ef8d1599ba875102c.png"
       roundImage
     >
       <div>Homepage</div>
     </GradientLayout>
   );
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return {
+    props: { artists },
+  };
 };
 
 export default Home;
