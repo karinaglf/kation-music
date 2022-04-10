2f153ddd55d6119fbac4ae0465733be0d2b3729e
Karina Freitas <karinaglf@gmail.com>
2022-04-10 01:30:03 +0100

    add gradient layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,18 @@
+import GradientLayout from "../components/gradientLayout";
+
 const Home = () => {
-  return <div> Home </div>;
+  return (
+    <GradientLayout
+      color="blue"
+      title="Karina Freitas"
+      subtitle="profile"
+      description="15 public playlists"
+      image="https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"
+      roundImage
+    >
+      <div>Homepage</div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
